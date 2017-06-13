5d9cebf9a76fecda6f714fa6a5401203186b2646
Manoj Kulkarni <manoj.kulkarni.3011@gmail.com>
2017-06-13 22:08:03 +0530

    Nuget packages are updated to latest version

--- WeatherApp/WeatherApp/Service/IOpenWeatherMapApi.cs
+++ WeatherApp/WeatherApp/Service/IOpenWeatherMapApi.cs
@@ -6,7 +6,7 @@ namespace WeatherApp.Service
 {
     public interface IOpenWeatherMapApi
     {
-        [Get("/weather?lat={latitude}&lon={longitude}&units={unit}&appid=<YOUR_KEY>")]
+        [Get("/weather?lat={latitude}&lon={longitude}&units={unit}&appid=fc9f6c524fc093759cd28d41fda89a1b")]
         Task<WeatherRoot> GetWeatherByLatitudeAndLongitude(double latitude, double longitude, string unit);
     }
 }
