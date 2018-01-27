464329b60a84d2393fd1fb583d6e675d822c1f5f
Manoj Kulkarni <manoj.kulkarni.3011@gmail.com>
2018-01-27 22:27:28 +0530

    Portable  library is replaced with .net standard library (#3)

---
+++ WeatherApp/WeatherApp/Services/IOpenWeatherMapApi.cs
@@ -0,0 +1,18 @@
+﻿using Refit;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    public interface IOpenWeatherMapApi
+    {
+        [Get("/weather?lat={latitude}&lon={longitude}&units={unit}&appid={appId}")]
+        Task<WeatherRoot> GetWeatherByLatitudeAndLongitudeAsync(double latitude, double longitude, string unit, string appId);
+
+        [Get("/weather?q={city}&units={unit}&appid={appId}")]
+        Task<WeatherRoot> GetWeatherByCityAsync(string city, string unit, string appId);
+
+        [Get("/weather?zip={zipCode}&units={unit}&appid={appId}")]
+        Task<WeatherRoot> GetWeatherByZipCodeAsync(string zipCode, string unit, string appId);
+    }
+}
